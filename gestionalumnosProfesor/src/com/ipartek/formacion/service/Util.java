2fa56d9ac9df635e05dcb4a23154256f569a69ea
urko villanueva <urko.informatica@gmail.com>
2016-06-24 08:22:41 +0200

    Excepciones y DNI

---
+++ gestionalumnosProfesor/src/com/ipartek/formacion/service/Util.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.service;
+
+public class Util {
+	public static boolean validarDni(String dni){
+		boolean valido = false;
+		dni = dni.toUpperCase();
+		
+		int nDni = Integer.parseInt(dni.substring(0, dni.length()-1));
+		char lDni = dni.substring(dni.length()-2, dni.length()-1).charAt(0);
+		if(calcularLetra(nDni)==lDni){
+			valido = true;
+		}
+			
+		return valido;
+	}
+	private static char calcularLetra(int nDni){
+		char letra = 0;
+		final char [] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'};
+		letra = letras[nDni % letras.length];
+		return letra;
+	}
+}
