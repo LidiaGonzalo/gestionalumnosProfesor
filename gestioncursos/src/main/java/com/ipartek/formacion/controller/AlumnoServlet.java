89e25c700a34e261f8e09781f724fb00d50d19a9
urko villanueva <urko.informatica@gmail.com>
2016-06-27 17:31:16 +0200

    Alumnos Servlet

--- gestioncursos/src/main/java/com/ipartek/formacion/controller/AlumnoServlet.java
+++ gestioncursos/src/main/java/com/ipartek/formacion/controller/AlumnoServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ipartek.formacion.pojo.Alumno;
 import com.ipartek.formacion.pojo.Curso;
 import com.ipartek.formacion.service.AlumnoService;
 import com.ipartek.formacion.service.AlumnoServiceImp;
@@ -23,21 +24,39 @@ public class AlumnoServlet extends HttpServlet {
 	 private int id = -1;
 	 private RequestDispatcher rd = null;
 	 private AlumnoService aService = new AlumnoServiceImp();
-	 private List<Curso> alumnos = null;
-	 private Curso alumno = null;   
+	 private List<Alumno> alumnos = null;
+	 private Alumno alumno = null;   
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		try{
+			id = Integer.parseInt(request.getParameter(Constantes.PAR_CODIGO));
+			getById(request);
+		}catch(Exception e){
+			getAll(request);
+		}
+		rd.forward(request, response); 
+	}
+
+	private void getAll(HttpServletRequest request) {
+		alumnos = aService.getAll();
+		request.setAttribute(Constantes.ATT_LISTADO_CURSOS, alumnos);
+		rd = request.getRequestDispatcher(Constantes.JSP_LISTADO_ALUMNOS);
+	}
+
+	private void getById(HttpServletRequest request) {
+		alumno = aService.getById(id);
+		request.setAttribute(Constantes.ATT_ALUMNO, alumno);
+		rd = request.getRequestDispatcher(Constantes.JSP_ALUMNO);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
